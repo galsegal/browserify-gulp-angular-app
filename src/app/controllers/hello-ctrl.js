ae2e3291769a8a246a423230b5fcefa587b087d6
Danny Rankevich <danny.ran@gmail.com>
2014-12-16 00:06:56 +0200

    add flickr photos

--- src/app/controllers/hello-ctrl.js
+++ src/app/controllers/hello-ctrl.js
@@ -2,19 +2,14 @@
 module.exports = function HelloCtrl($http, FlickrService) {
     'use strict';
     var vm = this;
-    vm.displayText = 'World!!';
-    FlickrService.get(function (resource) {
-        resource.toString();
-    });
-    vm.xhrData = {};
-    $http.get('http://www.reddit.com/r/nodejs.json')
-        .then(function (data) {
-            console.log('$http.get', arguments);
-            vm.xhrData.parsed = data.data.data.children.map(function (c) {
-                return {
-                    url: c.data.url,
-                    title: c.data.title,
-                };
-            });
+    vm.displayText = 'Flickr Photos';
+    vm.tags = 'forest';
+
+    vm.search = function () {
+        FlickrService.get({ tags: vm.tags }, function (resource) {
+            vm.photos = resource.items;
         });
+    };
+
+    vm.search();
 };
